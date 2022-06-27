af698abc361a8fd5d809181cefc78cc6080b1865
adgor <adgor000@gmail.com>
2022-06-27 21:05:39 +0200

    change icon

--- src/components/hero/HeroService.js
+++ src/components/hero/HeroService.js
@@ -7,10 +7,10 @@ export const HeroService = () => {
       <div className='relative mx-auto overflow-hidden px-4 py-16 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8 lg:py-20'>
         <div className='grid items-center pb-10 lg:grid-cols-2'>
           <div className='w-full'>
-            <h2 className='mb-6 max-w-lg uppercase text-3xl font-extrabold tracking-tight text-white sm:text-5xl'>
+            <h2 className='mb-6 max-w-lg uppercase text-3xl font-extrabold text-brand-purple sm:text-5xl'>
             your vision 
               <br />
-              <span className='font-bold'>into reality</span>
+              <span className='font-bold text-white'>into reality</span>
             </h2>
             <p className='px-0 text-lg text-white md:text-xl'>
               SiteBuilder, 1-click app installer or with a couple of clicks,
